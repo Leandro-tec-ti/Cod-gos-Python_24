290b429100a661b08153d4247007059f23b9ff38
Leandro-tec-ti <leandro.tec.ti@hotmail.com>
2024-10-02 19:32:11 -0300

    exemplo 1 atualização
    Melhorei o mapa da tesouro

--- Aula2/exe.py
+++ Aula2/exe.py
@@ -1,4 +1,7 @@
 #Esses serão os passos do pirata
+print("Este é o caminho para o mapa do tesouro")
 print("andar para direita")
 print("Andar para esquerda")
 print("andar para baixo")
+print("procure o x e cavar")
+
