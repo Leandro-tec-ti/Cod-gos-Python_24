b7ca6a4b457fd1abae0fe66f9474a13860a0d7cb
Leandro-tec-ti <leandro.tec.ti@hotmail.com>
2024-10-02 21:11:34 -0300

    Calculando IPVA do carro
    teste para ipva do carro

---
+++ Aula3/exercicio1.py
@@ -0,0 +1,7 @@
+#calculando o valor do ipva do carro
+print("Calculando o valor do IPVA do carro. ")
+valorcarro = float(input("Colocar o valor do carro? "))
+ipva = valorcarro * 0.04
+print(f" O valor do ipva é srobre o valor do carro é: {ipva}")
+
+
