a5514d0a06f20daef7f381336e42f283c1a8c353
Leandro-tec-ti <leandro.tec.ti@hotmail.com>
2024-10-02 21:50:20 -0300

    dever de casa
    exercicio casa

---
+++ Aula3/Dever de casa.py
@@ -0,0 +1,5 @@
+#Peça para o usuário o tamanho do aro da roda bike que ele quer comprar e com base nisso:
+#Recomende o Tamanho do guidão, da manete e do quadro da bike
+# O tamanho do guidão será metade do tamanho do aro da roda
+# O tamanho do manete será 1/4 do tamanho do aro da roda
+# O tamanho quadro da bike será o mesmo do aro da roda
---
+++ Aula3/exercicio3.py
@@ -0,0 +1,8 @@
+
+Coleira = float(input("Tamanho da coleira? "))
+sapato = Coleira*2 
+funcieira = Coleira*3
+
+
+print(f"O tamanho sapato é: {sapato:.2f} ")
+print(f"o tamanho da funcieira: {funcieira:.2f}")
