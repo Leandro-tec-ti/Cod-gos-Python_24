4da93e39e32decea8c970ca7d66a23d7b75c8da5
Leandro Fernandes <leandro.tec.ti@hotmail.com>
2024-10-04 19:29:04 +0000

    dever de casa

--- Aula3/Dever de casa.py
+++ Aula3/Dever de casa.py
@@ -1,5 +1,15 @@
 #Peça para o usuário o tamanho do aro da roda bike que ele quer comprar e com base nisso:
-#Recomende o Tamanho do guidão, da manete e do quadro da bike
+#Recomendo Tamanho do guidão, da manete e do quadro da bike
 # O tamanho do guidão será metade do tamanho do aro da roda
 # O tamanho do manete será 1/4 do tamanho do aro da roda
 # O tamanho quadro da bike será o mesmo do aro da roda
+
+
+#print("Qual tamanho do aro? ")
+aro = int(input("Qual tamanho do aro? "))
+Guidao = int(aro /2) 
+manete = int(aro /4)
+guadro = aro 
+print(f"Recomdamos que o tamanho do guidão seja: {Guidao} ")
+print(f"A manete que recomendamos é do tamanho: {manete}")
+print(f"O quadro terá que ser do tamanho: {guadro}")
