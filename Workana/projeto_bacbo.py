abc33bfa6fb73be2ee869de359b0eb0eb88b756b
Leandro Fernandes <leandro.tec.ti@hotmail.com>
2024-10-28 11:24:47 -0300

    Estudando em casa
    concluído

---
+++ Workana/projeto_bacbo.py
@@ -0,0 +1,19 @@
+import selenium
+#O programador precisa ter conhecimento em webscrap e socket.
+
+
+#Quero que o bot reconheça as cores na vertical de acordo com o histórico do jogo.
+#Quero configura-lo para ele entrar apartir da segunda cor como no modelo abaixo.
+
+#🔵🔴🔵
+#🔴🔵🔴
+#🔵🔴🔵
+#O bot reconhecerá as oportunidades de apostas após a segunda cor na vertical encontrada no histórico.
+#Obs: será cadastrada no bot outras regras como essa acima.
+
+matriz = [
+    ['  ','🔴','🔵'],
+    ['🔴','🔵','🔴'],
+    ['🔵','🔴','🔵']
+    ]
+linha = input("Qual a cor desejada, Vermelha / Azul? \n")
