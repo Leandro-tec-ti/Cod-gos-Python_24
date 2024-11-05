ce1325ecf2a18a946872174894e17726514750e7
Leandro-tec-ti <leandro.tec.ti@hotmail.com>
2024-11-04 21:54:20 -0300

    Aula 5
    front end senac

---
+++ Front-end/Aula_5/Exemplo2/exemplo2.js
@@ -0,0 +1,16 @@
+document.write('Olá Terra')
+// alert('Olá');
+alert('Olá')
+console.log("Olá tudo bem?")
+var variavel1;
+// alert(Variavel1);
+var variavel2 = 123;
+// alert (variavel2);
+console.log(variavel2);
+let variavel3 = 'Eu também sou uma variavel';
+// alert(variavel3)
+
+/* Comentario em javascript em multiplas linhas */
+document.write('Olá marte')
+document.write('<h1> Eu sou um titulo </h1>')
+document.write('<title> Titulo da pagina </title>')
