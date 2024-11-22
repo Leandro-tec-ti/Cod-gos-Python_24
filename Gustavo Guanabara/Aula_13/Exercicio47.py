0f1a68f5f8e2ae56637d2da58d2799a4d9b383c7
Leandro Fernandes <leandro.tec.ti@hotmail.com>
2024-11-22 00:09:59 -0300

    corrigindo inicio da aula 13
    Gustavo Guanabara

--- Gustavo Guanabara/Aula_13/Exercicio47.py
+++ Gustavo Guanabara/Aula_13/Exercicio47.py
@@ -1,5 +1,12 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
-for c in range(3-1, 52, 2):
+# Primeira Alternativa Feita por mim
+for c in range(2, 51, 2): 
     print(c)
 print("Fim")
+
+#segunda Alternativa feita no curso
+for c in range(1, 51):
+    if c % 2 == 0:
+        print(c, end= " ")
+print("Fim ")
--- Gustavo Guanabara/Aula_13/Exercicio48.py
+++ Gustavo Guanabara/Aula_13/Exercicio48.py
@@ -2,8 +2,11 @@
 # e que se encontram no intervalo de 1 até 500.
 
 s = 0
+cont = 0
 for c in range(1, 500, 2):
     if c % 3 == 0:
         print(f"Multiplos de 3 = {c}")
-        s += c
+        cont = cont + 1 #<<<--- conta a quantidade de numeros impares multiplos de 3 (mais um numero que foi achado)
+        s += c   #<<<--- faz o calculo dos numeros acomulados 
 print(f"A soma foi {s}")
+print(f"A quantidade de numeros é: {cont}")
